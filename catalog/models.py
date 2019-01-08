cd45cf1b9045e4aa3a77f2d6f6a8c307c9e05a75
Siyet <a.s.tseluyko@gmail.com>
2019-01-08 22:57:48 +0300

    правки

--- catalog/models.py
+++ catalog/models.py
@@ -12,14 +12,14 @@ class Base(models.Model):
     """
     Абстрактная базовая модель
     """
-    uid = models.UUIDField(verbose_name="Идентификатор", primary_key=True, default=uuid.uuid4, editable=False)
+    # uid = models.UUIDField(verbose_name="Идентификатор", primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Когда создано")
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Кем создано", editable=False, related_name="+")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Когда обновлено")
     updated_by = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Кем обновлено", editable=False, related_name="+")
     is_public = models.BooleanField("Опубликовано?", default=True)
     deleted = models.BooleanField("В архиве?", default=False, editable=False)
-    rev = models.CharField("Ревизия", default='1-{0}'.format(uuid.uuid4()), max_length=38, editable=False)
+    # rev = models.CharField("Ревизия", default='1-{0}'.format(uuid.uuid4()), max_length=38, editable=False)
     # json-delta пакет для работы с разностью в json; json_patch - функция пакета для применения изменений.
 
     class Meta:
@@ -54,15 +54,16 @@ class Category(Base):
     """
     Модель класса товаров
     """
-    subcategories = None
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, verbose_name="Родительский класс", related_name='childs')
+    childs = None
     title = models.CharField(max_length=255, verbose_name='Наименование')
     short_title = models.CharField(max_length=255, verbose_name='Краткое наименование', blank=True)
     
     def getAttributes(self):
-        if not self.subcategories:
+        if not self.childs:
             return []
         attrs = []
-        for subcat in self.subcategories.all():
+        for subcat in self.childs.all():
             _attrs = subcat.attributes if subcat.attributes else []
             diff = set(attrs) - set(_attrs)
             attrs = attrs + list(diff)
@@ -76,20 +77,20 @@ class Category(Base):
         verbose_name_plural = "Классы"
 
 
-class Subcategory(Base):
-    """
-    Модель Пподкласса товаров
-    """
-    title = models.CharField(max_length=255, verbose_name='Наименование')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Класс", related_name='subcategories')
-    short_title = models.CharField(max_length=255, verbose_name='Краткое наименование', blank=True)
+# class Subcategory(Base):
+#     """
+#     Модель Пподкласса товаров
+#     """
+#     title = models.CharField(max_length=255, verbose_name='Наименование')
+#     category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Класс", related_name='subcategories')
+#     short_title = models.CharField(max_length=255, verbose_name='Краткое наименование', blank=True)
     
-    def __str__(self):
-        return self.category.title + ' -> ' + self.title
+#     def __str__(self):
+#         return self.category.title + ' -> ' + self.title
 
-    class Meta:
-        verbose_name = "Подкласс"
-        verbose_name_plural = "Подклассы"
+#     class Meta:
+#         verbose_name = "Подкласс"
+#         verbose_name_plural = "Подклассы"
 
 
 class Attribute(Base):
@@ -112,7 +113,7 @@ class Attribute(Base):
     title = models.CharField(max_length=255, verbose_name='Наименование')
     type = models.CharField(max_length=13, choices=TYPES, verbose_name="Тип")
     priority = models.PositiveSmallIntegerField(verbose_name='Приоритет')
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.PROTECT, verbose_name="Подкласс", related_name='attributes')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Класс", related_name='attributes')
     
     def __str__(self):
         return self.title
@@ -127,7 +128,7 @@ class Product(Base):
     Модель товара
     """
     article = models.CharField(max_length=255, verbose_name='Артикул')
-    subcategory = models.ForeignKey(Subcategory, on_delete=models.PROTECT, verbose_name="Подкласс", related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Класс", related_name='products')
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.PROTECT, verbose_name="Производитель", related_name='products')
 
     def __str__(self):
@@ -146,6 +147,7 @@ class AttributeValue(Base):
     Модель значения атрибута
     """
     title = models.CharField(max_length=255, verbose_name='Значение')
+    product = 
     attribute = models.ForeignKey(Attribute, on_delete=models.PROTECT, verbose_name="Атрибут", related_name="values")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Атрибуты", related_name="attrs_values")
 
